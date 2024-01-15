c25d5fdc31004bb7e80918856520736c4cbf5f0a
walidtalal <alwaleedhawas@gmail.com>
2024-01-16 01:15:45 +0200

    register-page

--- config/config.php
+++ config/config.php
@@ -10,7 +10,7 @@ $pass = '';
 try {
     $conn = new PDO("mysql:host=$host;dbname=$dbname", $user, $pass);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    echo "Connected successfully";
+//    echo "Connected successfully";
 } catch (PDOException $e) {
     echo "Connection failed: " . $e->getMessage();
 }
