4f49565751ae0ba8929723ebc665d4b255ce96c3
walidtalal <alwaleedhawas@gmail.com>
2024-01-16 03:47:06 +0200

    Displaying-Products

--- index.php
+++ index.php
@@ -1,84 +1,27 @@
 <?php include "includes/header.php"?>
-        <div class="row mt-5">
-            <div class="col-lg-4 col-md-6 col-sm-10 offset-md-0 offset-sm-1">
-                <div class="card" >
-                    <img height="213px" class="card-img-top" src="images/node.png">
-                    <div class="card-body" >
-                        <h5 class="d-inline"><b>Node Basics</b> </h5>
-                        <h5 class="d-inline"><div class="text-muted d-inline">($10/item)</div></h5>
-                        <p>Monotonectally enable customized 
-                            growth strategies and 24/7 portals.  functional opportunities. </p>
-                         <a href="#"  class="btn btn-primary w-100 rounded my-2"> More<i class="fas fa-arrow-right"></i> </a>      
-     
-                    </div>
-                </div>
-            </div>
-<!--            --><?php //echo $_SESSION['username']?>
-            <br>
-            <div class="col-lg-4 col-md-6 col-sm-10 offset-md-0 offset-sm-1 mb-5">
-                <div class="card">
-                    <a href="http://localhost/bookstore/shopping/single.php"><img height="213px" class="card-img-top" src="images/django.png"></a>
-                    <div class="card-body">
-                    <h5 class="d-inline"><b>Django Basics</b> </h5>
-                        <h5 class="d-inline"><div class="text-muted d-inline">($20/item)</div></h5>
-                        <p>Monotonectally enable customized 
-                            growth strategies and 24/7 portals.  functional opportunities. </p>
-                       <a href="#"  class="btn btn-primary w-100 rounded my-2"> More<i class="fas fa-arrow-right"></i> </a>      
-                    </div>
-                </div>
-            </div>
-            <br>
-            <div class="col-lg-4 col-md-6 col-sm-10 offset-md-0 offset-sm-1">
-                <div class="card">
-                    <img     height="213px" class="card-img-top" src="images/django.png">
-                    <div class="card-body">
-                    <h5 class="d-inline"><b>Django Basics</b> </h5>
-                        <h5 class="d-inline"><div class="text-muted d-inline">($50/item)</div></h5>
-                        <p>Monotonectally enable customized 
-                            growth strategies and 24/7 portals.  functional opportunities. </p>
-                       <a href="#"  class="btn btn-primary w-100 rounded my-2"> More<i class="fas fa-arrow-right"></i> </a>      
-                    </div>
-                </div>
-            </div>
-            <br>
-            <div class="col-lg-4 col-md-6 col-sm-10 offset-md-0 offset-sm-1">
-                <div class="card">
-                    <img height="213px" class="card-img-top" src="images/django.png">
-                    <div class="card-body">
-                    <h5 class="d-inline"><b>Django Basics</b> </h5>
-                        <h5 class="d-inline"><div class="text-muted d-inline">($25/item)</div></h5>
-                        <p>Monotonectally enable customized 
-                            growth strategies and 24/7 portals.  functional opportunities. </p>
-                       <a href="#"  class="btn btn-primary w-100 rounded my-2"> More<i class="fas fa-arrow-right"></i> </a>       
-                    </div>
-                </div>
-            </div>
-            <br>
-            <div class="col-lg-4 col-md-6 col-sm-10 offset-md-0 offset-sm-1">
-                <div class="card">
-                    <img height="213px" class="card-img-top" src="images/html5.jpg">
-                    <div class="card-body">
-                    <h5 class="d-inline"><b>Html5 Basics</b> </h5>
-                        <h5 class="d-inline"><div class="text-muted d-inline">($20/item)</div></h5>
-                        <p>Monotonectally enable customized 
-                            growth strategies and 24/7 portals.  functional opportunities. </p>
-                        <a href="#"  class="btn btn-primary w-100 rounded my-2"> More<i class="fas fa-arrow-right"></i> </a>      
-                    </div>
-                </div>
-            </div>
-            <br>
+<?php include "config/config.php"?>
 
-            <div class="col-lg-4 col-md-6 col-sm-10 offset-md-0 offset-sm-1">
-                <div class="card d-relative" >
-                    <img height="213px" class="card-img-top" src="images/django.png">
-                    <div class="card-body" width="200px">
-                    <h5 class="d-inline"><b>Django Basics</b> </h5>
-                        <h5 class="d-inline"><div class="text-muted d-inline">($10/item)</div></h5>
-                        <p>Monotonectally enable customized 
-                            growth strategies and 24/7 portals.  functional opportunities. </p>
-                       <a href="#"  class="btn btn-primary w-100 rounded my-2"> More<i class="fas fa-arrow-right"></i> </a>      
+<?php
+$rows = $conn->query('select * from products where status = 1 ');
+$rows->execute();
+
+$allRows = $rows->fetchAll(PDO::FETCH_OBJ)
+?>
+        <div class="row mt-5">
+            <?php foreach ($allRows as $product):?>
+                <div class="col-lg-4 col-md-6 col-sm-10 offset-md-0 offset-sm-1">
+                    <div class="card" >
+                        <img height="213px" class="card-img-top" src="images/<?php echo $product->image;?>">
+                        <div class="card-body" >
+                            <h5 class="d-inline"><b><?php echo $product->name;?></b> </h5>
+                            <h5 class="d-inline"><div class="text-muted d-inline">($<?php echo $product->price;?>/item)</div></h5>
+                            <p><?php echo $product->description;?> </p>
+                             <a href="<?php echo APPURL?>/shopping/single.php?id=<?php echo $product->id;?>"  class="btn btn-primary w-100 rounded my-2"> More<i class="fas fa-arrow-right"></i> </a>
+
+                        </div>
                     </div>
-                </div>
             </div>
+            <br>
+            <?php endforeach;?>
          </div>
 <?php include "includes/footer.php"?>
