33df0b92c74d1992f92ed5aa7dd366492b76cbf8
walidtalal <alwaleedhawas@gmail.com>
2024-02-08 21:20:09 +0200

    Creating Index Page for Admins

--- admin-panel/index.php
+++ admin-panel/index.php
@@ -1,13 +1,30 @@
 <?php require "layouts/header.php"?>
 <?php require "../config/config.php"; ?>
 
+<?php
+
+$products = $conn->prepare("select count(*) as products_num from products");
+$products->execute();
+$allProducts = $products->fetch(PDO::FETCH_OBJ);
+
+$categories = $conn->prepare("select count(*) as categories_num from categories");
+$categories->execute();
+$allCategories = $categories->fetch(PDO::FETCH_OBJ);
+
+
+$admins = $conn->prepare("select count(*) as admins_num from admins");
+$admins->execute();
+$allAdmins = $admins->fetch(PDO::FETCH_OBJ);
+
+?>
+
 <div class="row">
         <div class="col-md-4">
           <div class="card">
             <div class="card-body">
               <h5 class="card-title">Products</h5>
               <!-- <h6 class="card-subtitle mb-2 text-muted">Bootstrap 4.0.0 Snippet by pradeep330</h6> -->
-              <p class="card-text">number of products: 8</p>
+              <p class="card-text">number of products: <?php echo $allProducts->products_num;?></p>
              
             </div>
           </div>
@@ -17,7 +34,7 @@
             <div class="card-body">
               <h5 class="card-title">Categories</h5>
               
-              <p class="card-text">number of categories: 4</p>
+              <p class="card-text">number of categories: <?php echo $allCategories->categories_num;?></p>
               
             </div>
           </div>
@@ -27,7 +44,7 @@
             <div class="card-body">
               <h5 class="card-title">Admins</h5>
               
-              <p class="card-text">number of admins: 3</p>
+              <p class="card-text">number of admins: <?php echo $allAdmins->admins_num;?></p>
               
             </div>
           </div>
